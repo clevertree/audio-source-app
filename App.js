a83b214603968907f9080ced94fb7177901ce318
Ari Asulin <ari.asulin@gmail.com>
2020-05-05 14:31:10 -0700

    updates

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {
   View,
   StatusBar,
-  SafeAreaView,
+  SafeAreaView, ScrollView,
   Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
 import MenuDrawer from 'react-native-side-drawer';
 import {ASPlayer} from './audio-source/player';
@@ -43,25 +43,9 @@ class App extends React.Component {
 
   render() {
     return (
-        <View>
+        <SafeAreaView>
           <ASComposer />
-          {/*<ASPlayer />*/}
-          {/*<MenuDrawer*/}
-          {/*  open={this.state.menuOpen}*/}
-          {/*  drawerContent={this.drawerContent()}*/}
-          {/*  drawerPercentage={55}*/}
-          {/*  animationTime={this.state.menuOpen ? 250 : 50}*/}
-          {/*  overlay={true}*/}
-          {/*  opacity={1.0}*/}
-          {/*  >*/}
-          {/*  <SafeAreaView>*/}
-          {/*    <TouchableWithoutFeedback onPress={this.state.menuOpen ? this.toggleMenu : null}>*/}
-          {/*      <View>*/}
-                {/*</View>*/}
-              {/*</TouchableWithoutFeedback>*/}
-            {/*</SafeAreaView>*/}
-          {/*</MenuDrawer>*/}
-        </View>
+        </SafeAreaView>
     );
   }
 }
