3be9ba47f1de4e8c87faa0baf54e720cdf796580
Ari Asulin <ari.asulin@gmail.com>
2020-05-07 16:18:54 -0700

    proxy html files

---
+++ android/app/src/main/assets/proxy/precache-manifest.f5d733b5f60ae3fc67f4bee7a6f698e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a077be484a34ffd064245945bb24941",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f26447a2cf5e88d87aa",
+    "url": "./static/css/main.1619a3c8.chunk.css"
+  },
+  {
+    "revision": "8161fa2af3bc1eb5949b",
+    "url": "./static/js/2.c761d30c.chunk.js"
+  },
+  {
+    "revision": "9871c1dba6089feb6aebe2bb837f00e5",
+    "url": "./static/js/2.c761d30c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f26447a2cf5e88d87aa",
+    "url": "./static/js/main.9160cf98.chunk.js"
+  },
+  {
+    "revision": "eb9ac87711fea29b50b5",
+    "url": "./static/js/runtime-main.7c6c85ed.js"
+  }
+]);
