26bcbc2b9385773552b38814dbae567fdeee4de4
Ari Asulin <ari.asulin@gmail.com>
2020-05-10 16:16:16 -0700

    ios html files

---
+++ ios/html/proxy/precache-manifest.a606a124dc809b3ef1f896e973ece028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2972d24401a093874cb1cd6481794ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e2a24cbd031e8f4bb77",
+    "url": "./static/css/main.4dd7c47e.chunk.css"
+  },
+  {
+    "revision": "cdaf8674769fb3195164",
+    "url": "./static/js/2.fac7fac6.chunk.js"
+  },
+  {
+    "revision": "d7207fc35c734fdb6973626819f7ee87",
+    "url": "./static/js/2.fac7fac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2a24cbd031e8f4bb77",
+    "url": "./static/js/main.40b28747.chunk.js"
+  },
+  {
+    "revision": "eb9ac87711fea29b50b5",
+    "url": "./static/js/runtime-main.7c6c85ed.js"
+  }
+]);
